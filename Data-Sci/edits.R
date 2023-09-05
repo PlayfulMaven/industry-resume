a0a689cfdc1ad147f424db836a6a3d6f99b9b1ca
Thomas Cade Abrams <cade4420@gmail.com>
2023-09-05 17:13:06 -0400

    initialized resume
    created left-hand side of resume, included skills bars, key job skills, and began changing color template

---
+++ Data-Sci/edits.R
@@ -0,0 +1,14 @@
+#----------------------------------------------------------------------------------------
+#  	PACKAGES AND OTHER DOCUMENT CONFIGURATIONS
+#----------------------------------------------------------------------------------------
+\documentclass[hidelinks,10pt,a4paper,]{twentysecondcv}
+
+
+# Right before "Some Stuff About Me" section
+% Skill bar section, each skill must have a value between 0 an 6 (float)
+\skills{{R/5.8},{Python/4},{SQL/0.5},{JavaScript/5.5}}
+
+% Skill text section, each skill must have a value between 0 an 6
+\skillstext{{tidyverse/},{NumPy/},{Jupyter Notebooks/},{SPSS/},{R Markdown/}}
+
+\makeprofile % Print the sidebar
